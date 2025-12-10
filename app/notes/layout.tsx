56f02be9b1547a9313fe077d6449663499673af7
Mykyta <nikita.borodin1212@gmail.com>
2025-12-10 15:09:13 +0200

    commit

--- app/notes/layout.tsx
+++ app/notes/layout.tsx
@@ -2,20 +2,12 @@ import "@/app/globals.css";
 
 export default function NotesLayout({
   children,
-  sidebar
 }: {
   children: React.ReactNode;
-  sidebar: React.ReactNode;
 }) {
   return (
-    <div>
-      <aside>
-        {sidebar}
-      </aside>
-
-      <main style={{ flex: 1 }}>
-        {children}
-      </main>
-    </div>
+    <main style={{ flex: 1 }}>
+      {children}
+    </main>
   );
 }
